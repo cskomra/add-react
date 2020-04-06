d3d7a5ba7826ac44360a692e17302d145849c771
Connie Skomra <connie@cskomra.com>
2020-04-06 10:38:31 -0400

    if language null, set to 'not code specific'

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,6 +7,6 @@ export const Card = props => (
         <img alt='project' src={`https://robohash.org/${props.project.id}?set=set2&size=180x180`} />
         <h3><a href={props.project.html_url}>{props.project.name}</a></h3>
         <p id='description'>{props.project.description}</p>
-        <p id='language'>{props.project.language}</p>
+        <p id='language'>{props.project.language ? props.project.language : "(not code specific)"}</p>
     </div>
 )
